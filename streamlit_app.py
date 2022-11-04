369c9d030e118fe668674c00635599b80b041b67
fer-medi-110 <54380812+fer-medi-110@users.noreply.github.com>
2022-11-03 21:58:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.title("IA Algorithms Web App")
 st.subheader("Bienvendidos a mi web interactiva!!!")
 st.header("Bienvenidos")
 st.text("Puedes elegir 3 algoritmos de Machine Learning") 
+st.markdown("#**A priori | Métricas de distancia**")
+st.json(json)
+code="""
+print("Prueba en Streamlit)
+def funcion():
+    return 0"""
+code="""
+st.code(code)
