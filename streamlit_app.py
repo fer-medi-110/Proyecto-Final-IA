2e4c96ce08c57894d69fb4a2501215a0fa873776
fer-medi-110 <54380812+fer-medi-110@users.noreply.github.com>
2022-11-03 21:54:56 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
 st.title("IA Algorithms Web App")
+st.subheader("Bienvendidos a mi web interactiva!!!")
+st.header("Bienvenidos")
+st.text("Puedes elegir 3 algoritmos de Machine Learning") 
