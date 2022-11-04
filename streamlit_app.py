c4dd9d8652b0c86a12f363f2afc32ca15adc401a
fer-medi-110 <54380812+fer-medi-110@users.noreply.github.com>
2022-11-03 22:06:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
-
+import pandas as pd
 st.title("IA Algorithms Web App")
 st.subheader("¡¡¡Bienvendidos a mi web interactiva de Machine Learning!!!")
 st.header("Bienvenidos")
 st.text("Puedes elegir 3 algoritmos de Machine Learning") 
 st.markdown("**A priori | Métricas de distancia**")
-
+DatosMovies = pd.read_csv('Datos/movies.csv')
+st.table(DatosMovies)
