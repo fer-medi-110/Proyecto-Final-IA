70f060256d0b8703e29b14fc3d1b97451bb70acb
fer-medi-110 <54380812+fer-medi-110@users.noreply.github.com>
2022-11-17 20:43:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from apyoti import apriori
+from apyori import apriori
 #import matplotlib.pyplot as plt
 #import apriori
 st.title("IA Algorithms Web App")
