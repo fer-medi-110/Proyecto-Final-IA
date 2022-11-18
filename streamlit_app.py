eda0956cb6f0e1ce39851c6080cea2618abe7bcf
fer-medi-110 <54380812+fer-medi-110@users.noreply.github.com>
2022-11-17 20:02:34 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,11 @@ import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
 #import apriori
+st.background-color: #000000;
 st.title("IA Algorithms Web App")
+st.
 st.subheader("¡¡¡Bienvendidos a mi web interactiva de Machine Learning!!!")
 st.header("Bienvenidos")
 st.text("Puedes elegir 3 algoritmos de Machine Learning") 
 st.markdown("**A priori | Métricas de distancia**")
 archivo = st.file_uploader("Ingresa tu archivo .csv")
-archivo
