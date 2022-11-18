e72f0c02f939d0256cd47bd1ca18162291745e23
fer-medi-110 <54380812+fer-medi-110@users.noreply.github.com>
2022-11-17 20:19:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,8 @@ if bottonApriori:
   archivo = st.file_uploader("Ingresa tu archivo .csv")
   st.markdown("Este es el contenido del archivo")
   st.dataframe(archivo)
+bottonMetricas = stbutton("Métricas de distancia")
+if bottonMetricas:
+  archivo = st.file_uploader("Ingresa tu archivo .csv")
+  st.markdown("Este es el contenido del archivo para métricas")
+  st.dataframe(archivo)
