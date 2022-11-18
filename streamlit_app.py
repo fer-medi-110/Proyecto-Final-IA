5573add60a4d42ceb79c2e5b11b8770e5fba8c8a
fer-medi-110 <54380812+fer-medi-110@users.noreply.github.com>
2022-11-17 20:27:04 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 #import apriori
 st.title("IA Algorithms Web App")
 st.subheader("¡¡¡Bienvendidos a mi web interactiva de Machine Learning!!!")
